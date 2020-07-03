6fb85c538f42f9685be7d95b09752067cca70762
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-03 15:54:27 -0500

    Creando servidor de Express y conectadose a MongoDB

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("desde index.js");
+const express = require("express");
+const conectarDB = require("./config/db");
+
+// Crear servidor
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//Definir la pagina principal
+// app.get("/", (req, res) => {
+//   res.send("Hola mundo");
+// });
+
+// arrancar la app
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT} `);
+});
