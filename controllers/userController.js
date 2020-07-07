e59ef739c5e15a416a7a340b79118b322e5ed1d7
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-07 12:54:29 -0500

    Validando email de usuarios y hasheando password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,29 @@
 const Usuario = require("../models/User");
+const bcryptjs = require("bcryptjs");
 
 exports.crearUsuario = async (req, res) => {
+  // extraer email y password
+  const { email, password } = req.body;
+
   try {
-    let usuario;
+    //Revisar que el usuario registrado sea unico
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
+    }
+
     // crea el nuevo usuario
     usuario = new Usuario(req.body);
+
+    //Hashear el password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
 
     // guardar usuario
     await usuario.save();
     // Mensaje de confirmacion
-    res.send("Usuario creado correctamente");
+    res.json({ msg: "Usuario creado correctamente" });
   } catch (error) {
     console.log(error);
     res.status(400).send("Hubo un error");
