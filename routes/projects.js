a650ca1759b598a3519b70fe9a6d7dc5fa04a143
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-08 14:18:39 -0500

    Validacion a los proyectos

--- routes/projects.js
+++ routes/projects.js
@@ -2,10 +2,20 @@ const express = require("express");
 const router = express.Router();
 const proyectoController = require("../controllers/projectController");
 const auth = require("../middleware/auth");
+const { check } = require("express-validator");
 
 // Crea proyectos
 // api/proyectos
-router.post("/", auth, proyectoController.crearProyecto);
+router.post(
+  "/",
+  auth,
+  [
+    check("nombre", "El nombre del proyecto es obligatorio")
+      .not()
+      .isEmpty(),
+  ],
+  proyectoController.crearProyecto
+);
 router.get("/", auth, proyectoController.crearProyecto);
 
 module.exports = router;
