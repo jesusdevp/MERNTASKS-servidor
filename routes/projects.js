a36b0575d50c39b88a46c9692a184d54cba521b2
Jesus Ramirez <jesusdevelp@gmail.com>
2020-07-08 15:20:38 -0500

    Editar proyectos

--- routes/projects.js
+++ routes/projects.js
@@ -16,6 +16,20 @@ router.post(
   ],
   proyectoController.crearProyecto
 );
+
+// Obtener todos los proyectos
 router.get("/", auth, proyectoController.obtenerProyectos);
+
+// Actualizar proyecto via ID
+router.put(
+  "/:id",
+  auth,
+  [
+    check("nombre", "El nombre del proyecto es obligatorio")
+      .not()
+      .isEmpty(),
+  ],
+  proyectoController.actualizarProyecto
+);
 
 module.exports = router;
